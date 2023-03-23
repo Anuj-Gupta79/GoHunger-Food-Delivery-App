fc719493772cba475b0911570277fc5f62fd6fa5
Anuj-Gupta79 <anujgupta7906@gmail.com>
2023-03-23 14:05:36 +0530

    Working on Navbar

--- frontendcode/src/App.js
+++ frontendcode/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import HomePage from './Screen/HomePage';
+import Login from './Screen/Login';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
+    <Router>
+    <div>
+      <Routes>
+          <Route exact path='/' element={<HomePage/>}/>
+          <Route exact path='/login' element={<Login/>}/>
+      </Routes>
     </div>
+  </Router>
   );
 }
 
--- frontendcode/src/Components/Navbar.jsx
+++ frontendcode/src/Components/Navbar.jsx
@@ -1,44 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-danger position-sticky">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            Navbar
-          </a>
+          <Link className="navbar-brand fs-1" to="/">
+            GoHunger
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <a className="nav-link active" aria-current="page" href="/">
-                Home
-              </a>
-              <a className="nav-link" href="/">
-                Features
-              </a>
-              <a className="nav-link" href="/">
-                Pricing
-              </a>
-              <a
-                className="nav-link disabled"
-                href="/"
-                tabindex="-1"
-                aria-disabled="true"
-              >
-                Disabled
-              </a>
-            </div>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
