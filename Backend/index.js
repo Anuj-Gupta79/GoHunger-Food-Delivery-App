d91786edf2984705cb4d7ed5b445300c66e2701f
Anuj-Gupta79 <anujgupta7906>
2023-05-26 16:09:48 +0530

    Backend Deploy

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ import OrderRoutes from "./Routes/OrderData.js";
 import LocationRoutes from './/Routes/GetLocation.js';
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.use(express.json());
 
 mongodb();
