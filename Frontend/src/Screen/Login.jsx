0bfbc7993c01e5dbf0335b1a5e5ecba92b8a44ee
Anuj-Gupta79 <anujgupta7906>
2023-05-26 12:31:07 +0530

    Backend Deploy

--- Frontend/src/Screen/Login.jsx
+++ Frontend/src/Screen/Login.jsx
@@ -12,7 +12,7 @@ export default function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:3001/GoHunger/logIn", {
+    const response = await fetch("https://go-hunger-backend-e2cn.onrender.com/GoHunger/logIn", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
